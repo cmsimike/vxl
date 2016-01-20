4ec9428b3239450824012c9f7356ad4cae204e18
Maxwell Flynn <tinfoilboy@gmail.com>
2016-01-19 22:12:32 -0500

    make the glViewport command actually use width and height, instead of double width.

--- vxl/vxl.cpp
+++ vxl/vxl.cpp
@@ -11,7 +11,7 @@ vxl::vxl() :
 	m_width(640),
 	m_height(480),
 	m_title("vxl"),
-	m_camera((float) m_width, (float) m_height) {
+	m_camera(static_cast<float>(m_width), static_cast<float>(m_height)) {
 }
 
 vxl::~vxl() {
@@ -38,9 +38,12 @@ int vxl::Run() {
 }
 
 void vxl::Resize(int width, int height) {
+	// set the width and height just in case
 	m_width = width;
 	m_height = height;
-	glViewport(0, 0, width, height);
+	// reset the viewport size to the new framebuffer width and height
+	glViewport(0, 0, static_cast<GLsizei>(m_width), static_cast<GLsizei>(m_height));
+	// resize the camera
 	m_camera.Resize(static_cast<float>(width), static_cast<float>(height));
 }
 
@@ -49,10 +52,14 @@ int vxl::_CreateWindow() {
 		std::cerr << "GLFW failed to initialize!" << std::endl;
 		return -1;
 	}
+	// make this window use OpenGL 3.2
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	// use the core profile so that we can have all of the goodness
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// make sure we can use new features if needed
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	// make this a resizable window
 	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
 	this->m_window = glfwCreateWindow(m_width, m_height, m_title.c_str(), NULL, NULL);
 	if (!m_window) {
@@ -71,12 +78,13 @@ int vxl::_CreateWindow() {
 		std::cerr << "GLEW failed to initialize!" << std::endl;
 		return -3;
 	}
+	// enable some things for correct rendering
 	_EnableGL();
 	return 0;
 }
 
 void vxl::_EnableGL() {
-	glViewport(0, 0, static_cast<GLsizei>(m_width), static_cast<GLsizei>(m_width));
+	glViewport(0, 0, static_cast<GLsizei>(m_width), static_cast<GLsizei>(m_height));
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LESS);
 }
@@ -84,7 +92,7 @@ void vxl::_EnableGL() {
 void vxl::_Init() {
 	m_defaultShader = new Shader(SHADER_DIR + std::string("vertex.glsl"), SHADER_DIR + std::string("fragment.glsl"));
 	m_world = new World();
-	m_camera.Translate(0.0f, -32.0f, -500.0f);
+	m_camera.Translate(0.0f, 0.0f, -500.0f);
 }
 
 void vxl::_Render() {
